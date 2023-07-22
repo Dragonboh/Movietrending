d5c0a0e5d31e33ff66204f87fb103019b7aace1e
Bogdan Pankevych <>
2023-07-22 22:30:20 +0300

    - add new folders for files - add LaunchScreen image - moved files to folder - delete SceneDelegate and Main Storyboard

---
+++ Movietrending/Movietrending/Controllers/MainViewController/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  Movietrending
+//
+//  Created by Bogdan Pankevych on 19.07.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "Main ViewController"
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
