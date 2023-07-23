5f9a6da4f9bec9c0abd383602fb4a44bf30c285c
Bogdan Pankevych <>
2023-07-23 19:03:44 +0300

    - added some template code for MainViewModel - create extension for MainViewController for UITableview delegate and datasource - create template functions in MainViewModel for table view.

--- Movietrending/Movietrending/Controllers/MainViewController/MainViewController.swift
+++ Movietrending/Movietrending/Controllers/MainViewController/MainViewController.swift
@@ -9,22 +9,20 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    //ViewModel:
+    var viewModel: MainViewModel = MainViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        conficView()
+    }
+    
+    func conficView() {
         self.title = "Main ViewController"
+        self.view.backgroundColor = .red
+        
+        setupTableView()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ Movietrending/Movietrending/ViewModels/MainViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewModel.swift
+//  Movietrending
+//
+//  Created by Bogdan Pankevych on 23.07.2023.
+//
+
+import Foundation
+
+class MainViewModel {
+    
+    func numberOfSections() -> Int {
+        1
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+        10
+    }
+    
+    
+}
